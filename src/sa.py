4d59e2dd4e993b745ac5e656f53913c8b261bd36
marse00 <gmmarcelino8@gmail.com>
2022-10-24 22:17:12 +0200

    Implemented Suffix array construction The construction algorithm is the naive one using radix sort

--- src/sa.py
+++ src/sa.py
@@ -11,5 +11,33 @@ def main():
           f"in genome {args.genome.name}")
 
 
+def radix_sort(lst: list[str]):
+    maximum = max(lst, key = len)
+
+    for place in range(len(maximum),0, -1):
+        lst = counting_sort(lst, place - 1)
+    
+    lst = [len(maximum) - len(suffix) for suffix in lst]
+    return lst
+            
+
+def counting_sort(lst: list[str], place):
+    maximum = max(lst, key = len)
+    counts = dict.fromkeys(["$",*maximum],[])
+
+    for string_index in range(len(lst)):
+        if place >= len(lst[string_index]):
+            counts["$"] = [*counts["$"], lst[string_index]]
+            
+        else:
+            counts[lst[string_index][place]] = [*counts[lst[string_index][place]], lst[string_index]]
+
+    ordered = []
+    for key in counts:
+        ordered += counts[key]
+    return ordered
+    
+    
 if __name__ == '__main__':
-    main()
+    radix_sort(["abab$","bab$","ab$","b$","$"])
+    #main()
